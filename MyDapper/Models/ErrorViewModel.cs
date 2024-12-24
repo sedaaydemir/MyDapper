24e29871b8a2290cb9b7cd49fa7abd7ba0fe9a78
Seda Aydemir <seda.aydemir7@gmail.com>
2024-12-24 22:23:32 +0300

    Add project files.

---
+++ MyDapper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
